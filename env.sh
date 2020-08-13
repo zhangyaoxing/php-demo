594ba9c8aada9d9ad0bf421d77443e43722dae2a
Yaoxing <yaoxing.zhang@gmail.com>
2020-08-13 21:28:35 +0800

    * fix permission issue

--- env.sh
+++ env.sh
@@ -5,13 +5,13 @@ echo "extension=mongodb.so" | sudo tee /etc/php.d/50-mongodb.ini
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+sudo php composer-setup.php --filename=composer --install-dir=/usr/bin
 php -r "unlink('composer-setup.php');"
-./composer.phar require mongodb/mongodb
 
-sudo mkdir -p /usr/share/nginx/html/php-demo
-sudo cp *.php /usr/share/nginx/html/php-demo/
-sudo cp -R vendor /usr/share/nginx/html/php-demo/
+composer require mongodb/mongodb
+cd ..
+sudo rm -rf /usr/share/nginx/html/php-demo
+cp -R php-demo /usr/share/nginx/html/
 
 sudo service nginx restart
 sudo service php-fpm restart
