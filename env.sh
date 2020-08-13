59c7afbfacda9b38bd5af00e07b7f3560f44cd85
Yaoxing <yaoxing.zhang@gmail.com>
2020-08-13 20:20:56 +0800

    * fix a permission issue

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum install -y php php-devel php-pear php-json php-zip make nginx unzip openssl-devel
 sudo pecl install mongodb
-sudo echo "extension=mongodb.so" >> /etc/php.d/50-mongodb.ini
+echo "extension=mongodb.so" | sudo tee -a /etc/php.d/50-mongodb.ini
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
