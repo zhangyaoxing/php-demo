d0f98a23538bf9768994df227565dce58de19abf
Yaoxing <yaoxing.zhang@gmail.com>
2020-08-17 16:25:09 +0800

    * do not veryfy composer

--- env.sh
+++ env.sh
@@ -5,7 +5,6 @@ sudo pecl install mongodb
 echo "extension=mongodb.so" | sudo tee /etc/php.d/50-mongodb.ini
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --filename=composer --install-dir=/usr/bin
 php -r "unlink('composer-setup.php');"
 
