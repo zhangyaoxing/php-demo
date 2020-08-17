9fedc38784d8b8603ffc6f2d8431d110ff1291a9
Yaoxing <yaoxing.zhang@gmail.com>
2020-08-17 16:18:22 +0800

    + add demo for legacy driver

--- demo-legacy.php
+++ demo-legacy.php
@@ -1,15 +1,11 @@
 <?php
-require_once __DIR__ . "/vendor/autoload.php";
-
-$client = new MongoDB\Client(
-    "mongodb://test:M0ngoDB20@cluster0-shard-00-00.wy1z6.mongodb.net:27017,cluster0-shard-00-01.wy1z6.mongodb.net:27017,cluster0-shard-00-02.wy1z6.mongodb.net:27017/sample_analytics?ssl=true&replicaSet=atlas-vnfeuv-shard-0&authSource=admin"
-);
-$db = $client->sample_airbnb;
-$collection = $db->listingsAndReviews;
-$cursor = $collection->aggregate([
-    ['$sample' => ['size' => 1]]
-])->toArray();
+$connection = new MongoClient( "mongodb://test:M0ngoDB20@cluster0-shard-00-00.wy1z6.mongodb.net:27017,cluster0-shard-00-01.wy1z6.mongodb.net:27017,cluster0-shard-00-02.wy1z6.mongodb.net:27017/sample_analytics?ssl=true&replicaSet=atlas-vnfeuv-shard-0&authSource=admin" );
+$db = $connection->sample_analytics;
+$collection = $db->accounts;
+$cursor = $collection->aggregate(array(array('$sample' => array('size' => 1))), 
+    array("cursor" => array("batchSize" => 10)));
 ?>
+
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -19,12 +15,12 @@ $cursor = $collection->aggregate([
             <div class="card">
                 <div class="card-body">
                     <h1 class="card-title">
-                        Random Document Selected from sample_airbnb.listingsAndReviews
+                        Random Document Selected from sample_analytics.accounts
                     </h1>
                     <hr />
                     <div class="card-text">
                         <pre><code><?php
-                        print_r(json_encode($cursor, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT));
+                        print_r($cursor["cursor"]["firstBatch"]);
                         ?></code></pre>
                     </div>
                 </div>
